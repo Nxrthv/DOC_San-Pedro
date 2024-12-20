5b060c5eec085cf4089d06e1093325db55e1f22a
AndresVargas0 <1458586@senati.pe>
2024-12-20 08:53:31 -0500

    Final

--- JavaScript/Funciones.js
+++ JavaScript/Funciones.js
@@ -7,11 +7,26 @@ function CT(){
     window.location.href = ('/Templates/CT.html')
 }
 
+function CV() {
+    window.location.href = ('/Templates/CV.html')
+}
+
 // Funciones
+
+//Sweet Alert
+function error (){
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Error al Insertar los datos",
+    });
+}
+
 // Guardar datos de la tabla en cache
 function guardarDatosEnLocalStorage() {
     const tableBody = document.querySelector('table tbody');
     localStorage.setItem('datosTabla', tableBody.innerHTML);
+    insertar()
 }
 
 // Cargar datos al iniciar la página
